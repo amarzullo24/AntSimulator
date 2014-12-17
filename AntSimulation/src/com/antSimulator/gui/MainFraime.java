891d61d0c8f6e1ae2f2137ed4fd7e6f413fdffc3
Marco <Marco@Marco-HP>
2014-12-17 13:19:19 +0100

    Fatto

--- AntSimulation/src/com/antSimulator/gui/MainFraime.java
+++ AntSimulation/src/com/antSimulator/gui/MainFraime.java
@@ -1,7 +1,15 @@
 package com.antSimulator.gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class MainFraime extends Application{
 
-public class MainFraime {
+	private String a;
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
